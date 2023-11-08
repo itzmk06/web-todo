dbdce8340702710763244f01f187fd0443ea1fee
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2023-11-08 17:48:30 +0530

    added new files

---
+++ app1.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from functions1 import get_todo,write_todo
+
+def add_todo():
+  todos=get_todo()
+  todos.append((st.session_state["new_todo"]+'\n').capitalize())
+  write_todo(todos)
+  st.session_state["new_todo"]=""
+st.title("Listify")
+todos=get_todo()
+for index,todo in enumerate(todos):
+  checkbox=st.checkbox(todo,key=todo)
+  if checkbox:
+    todos.pop(index)
+    del st.session_state[todo]
+    write_todo(todos)
+    st.experimental_rerun()
+st.text_input(label="",placeholder="Type in new todo...",on_change=add_todo,key="new_todo")
